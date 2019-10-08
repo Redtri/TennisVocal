1f664b922c772370f237f9a882c81372f21a0408
Guillaume <redtri@outlook.com>
2019-10-08 10:47:15 +0200

    [ADD] Micro testing class

---
+++ Assets/Scripts/Micro.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class Micro : MonoBehaviour
+{
+    public Microphone micro;
+    public AudioSource source;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        source = this.GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            source.clip = Microphone.Start("", false, 5, 44100);
+        }
+        if (Input.GetKeyDown(KeyCode.E)) {
+            if (!source.isPlaying && source.clip != null) {
+                source.Play();
+            }
+        }
+    }
+}
