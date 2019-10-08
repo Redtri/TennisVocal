5425990b976c455ec57f583107bc5b3bb499b243
Nicolas <n.leray94@gmail.com>
2019-10-08 14:47:46 +0200

    paddle + terrain

---
+++ Assets/Scripts/Paddle.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour
+{
+	[SerializeField]
+	private Terrain terrain;
+	[SerializeField]
+	private int caseNumber = 10;
+	[SerializeField]
+	private int startCase = 5;
+
+
+	private int _currentCase;
+	private Vector3 _startPos;
+
+	private void Start()
+	{
+		_currentCase = startCase;
+		_startPos = transform.position;
+	}
+
+	public void Move(int direction)
+	{
+		_currentCase += direction;
+		_currentCase = Mathf.Clamp(_currentCase, 0, caseNumber);
+	}
+
+	private Vector3 GetPosition()
+	{
+		float f = (float)_currentCase / (float)caseNumber;
+		Debug.Log(f);
+		return _startPos + Vector3.right * Mathf.Lerp(0, terrain.size.x,f);
+	}
+
+    void Update()
+    {
+		transform.position = GetPosition();
+    }
+}
---
+++ Assets/Scripts/Terrain.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Terrain : MonoBehaviour
+{
+	[SerializeField]
+	private Vector2 _size = new Vector2(10,12);
+	public Vector2 size { get { return _size; } }
+}
