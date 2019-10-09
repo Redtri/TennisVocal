c73c934043a836388d0c1f9dc04cb1956ae3ba5c
Guillaume <redtri@outlook.com>
2019-10-09 18:57:06 +0200

    [ADD] Pause Menu + Game Manager

---
+++ Assets/Scripts/Manager/GameManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject canvas;
+    private bool isPaused;
+    
+    // Start is called before the first frame update
+    void Start() {
+        isPaused = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            PauseResume();
+        }
+    }
+
+    public void PauseResume() {
+        if (isPaused) {
+            isPaused = false;
+            Time.timeScale = 1f;
+            canvas.SetActive(false);
+        } else {
+            isPaused = true;
+            Time.timeScale = 0f;
+            canvas.SetActive(true);
+        }
+    }
+}
