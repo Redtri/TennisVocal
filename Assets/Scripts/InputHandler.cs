a7ea695940ccc093d36e214d2a57db19e09852e5
Nicolas <n.leray94@gmail.com>
2019-10-09 16:39:19 +0200

    déso j'ai push trop vite

--- Assets/Scripts/InputHandler.cs
+++ Assets/Scripts/InputHandler.cs
@@ -66,7 +66,7 @@ public class InputHandler : MonoBehaviour, IInput {
 
 		RefreshAudioSpectrum();
 		UpdateInput();
-		paddle.Move(_axis);
+		//paddle.Move(_axis);
        // HandleCapture();
     }
 
