a5c31b1ddb6b2ba00af3aa2b5c09bf3b515fdd3f
Guillaume <redtri@outlook.com>
2019-10-10 10:55:06 +0200

    [UPDATE] Sounds menu

---
+++ Assets/SoundButton.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SoundButton : MonoBehaviour {
+
+    public AK.Wwise.Event wwClickEvent;
+    public AK.Wwise.Event wwHoverEvent;
+
+    private void OnEnable() {
+        GetComponent<Button>().onClick.AddListener(PressSound);
+    }
+
+    private void OnDisable() {
+        GetComponent<Button>().onClick.RemoveListener(PressSound);
+    }
+
+    private void PressSound() {
+        wwClickEvent.Post(gameObject);
+    }
+
+    public void HoverSound() {
+        wwHoverEvent.Post(gameObject);
+    }
+}
