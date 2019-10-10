90e549100af4f5091eaadc56dd7209b3cfa190f6
Guillaume <redtri@outlook.com>
2019-10-10 15:45:53 +0200

    [ADD] Troisième audio script dégueulasse

---
+++ Assets/SoundTrigger2.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundTrigger2 : MonoBehaviour {
+    public AK.Wwise.Event wwSound;
+
+    public void PlayR() {
+        wwSound.Post(gameObject);
+    }
+}
