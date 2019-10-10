51613c555d7d4e9a1e42878d8c7b27e9452f9d18
Guillaume <redtri@outlook.com>
2019-10-10 15:12:08 +0200

    [ADD] Animation menu SPLASH

--- Assets/Scripts/UI/UI_MainMenu.cs
+++ Assets/Scripts/UI/UI_MainMenu.cs
@@ -1,10 +1,13 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
 
 public class UI_MainMenu : MonoBehaviour
 {
     public GameObject mainMenu;
     public GameObject credits;
+    public GameObject splash;
     public Button playButton;
 
 
@@ -12,12 +15,19 @@ public class UI_MainMenu : MonoBehaviour
 	{
 		GameManager.instance.onPlay += StopSound;
         playButton.onClick.AddListener(PlayEvent);
-	}
+        splash.SetActive(true);
+        StartCoroutine(Splash());
+    }
 
-	private void OnDisable()
-	{
-		GameManager.instance.onPlay -= StopSound;
+    private void OnDisable() {
+        GameManager.instance.onPlay -= StopSound;
         playButton.onClick.RemoveListener(PlayEvent);
+    }
+
+
+    private IEnumerator Splash() {
+        yield return new WaitForSeconds(4.6f);
+        splash.SetActive(false);
     }
 
 	public void Menu() {
